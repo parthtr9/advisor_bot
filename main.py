bd861d668b2aa1672a7bd997d246a619a9568bf2
Parth Trivedi <154461332+parthtr9@users.noreply.github.com>
2025-05-08 16:19:19 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,73 @@
+import os
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.vectorstores import InMemoryVectorStore
+from langchain_community.document_loaders import PyPDFLoader
+from langchain import hub
+from langchain_core.documents import Document
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langgraph.graph import START, StateGraph
+from typing_extensions import List, TypedDict
+
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_API_KEY"] = "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"
+os.environ["GOOGLE_API_KEY"] = "AIzaSyCn6-OXpvGTNMHlBxw6NrgebC1giziRLrQ"
+
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro")
+
+embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+
+vector_store = InMemoryVectorStore(embeddings)
+
+# Load and chunk contents of the blog
+loader = PyPDFLoader(r"/Users/parth/Desktop/Internship/PProject/RAG/something.pdf")
+docs = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+all_splits = text_splitter.split_documents(docs)
+
+# Index chunks
+_ = vector_store.add_documents(documents=all_splits)
+
+# Define prompt for question-answering
+prompt = hub.pull("rlm/rag-prompt")
+
+
+# Define state for application
+class State(TypedDict):
+    question: str
+    context: List[Document]
+    answer: str
+
+
+# Define application steps
+def retrieve(state: State):
+    retrieved_docs = vector_store.similarity_search(state["question"])
+    return {"context": retrieved_docs}
+
+
+def generate(state: State):
+    docs_content = "\n\n".join(doc.page_content for doc in state["context"])
+    messages = prompt.invoke({"question": state["question"], "context": docs_content})
+    response = llm.invoke(messages)
+    return {"answer": response.content}
+
+
+# Compile application and test
+graph_builder = StateGraph(State).add_sequence([retrieve, generate])
+graph_builder.add_edge(START, "retrieve")
+graph = graph_builder.compile()
+
+import streamlit as st
+
+
+st.title("Advisor")
+main_placeholder = st.text_input("Enter your query")
+process = st.button("Send")
+
+if process:
+  query = main_placeholder
+  if query:
+        response = graph.invoke({"question": query})
+        answer = response["answer"]
+        st.write(answer)
